f1019e5c4bb160a4242af15a81e143531839ab36
Rohit Sharma <54910136+jason7531@users.noreply.github.com>
2020-07-13 19:41:45 +0530

    Update keys_dev.js

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://jason2004:sharma2004@cluster0.ugnh0.mongodb.net/yelp_camp?retryWrites=true&w=majority",
+    mongoURI: "<mongo atlas generated url>",
     secretOrKey: "Secret",
 };
